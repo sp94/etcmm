499f6d619650538f36b7c94a95d08a35100af91c
S J Palmer <samuelpalmer94@gmail.com>
2019-03-26 18:14:58 +0000

    Rename '2d' to 'fem'

--- fem/run_calculations.jl
+++ fem/run_calculations.jl
@@ -46,66 +46,59 @@ end
 ############# Figure 2a ##############
 ######################################
 
-# println("Begin Figure 2a; hexagonal lattice, d=38nm, L=40nm (G=2nm); both polarisations")
-# jldopen("./data/figure2a.jld", "w") do f
-#     lam0s = union(2:0.5*sf:10, 10:2*sf:50, 50:5*sf:100, 100:10*sf:240) # microns
-#     neffs_TM = ComplexF64[]
-#     neffs_TE = ComplexF64[]
-#     ns_bulk = ComplexF64[]
-#     @showprogress for lam0 in lam0s
-#         k0 = 2pi/lam0/1000 # [1/nm]
-#         ep = LorentzDrudeMetals.Ti[lam0]
-#         n = sqrt(ep)
-#         push!(ns_bulk, n)
-#         delta_s = lam0 / (-4pi*imag(n)) * 1000 # skin depth [nm]
-#         msh = make_msh_hexlat_cyl(38, 38, 40, 40*sqrt(3), delta_s, 20)
-#         m = Mesh(msh, [1.0,ep], [1.0,1.0])
-#         neff = homogenize(m, k0, :TM)[1][1]
-#         push!(neffs_TM, neff)
-#         neff = homogenize(m, k0, :TE)[1][1]
-#         push!(neffs_TE, neff)
-#     end
-#     write(f, "lam0s", lam0s)
-#     write(f, "neffs_TM", neffs_TM)
-#     write(f, "neffs_TE", neffs_TE)
-#     write(f, "ns_bulk", ns_bulk)
-# end
-
-
-######################################
-############# Figure 2c ##############
-######################################
-
-#println("Begin Figure 2c; ")
+println("Begin Figure 2a; hexagonal lattice, d=38nm, L=40nm (G=2nm); both polarisations")
+jldopen("./data/figure2a.jld", "w") do f
+    lam0s = union(2:0.5*sf:10, 10:2*sf:50, 50:5*sf:100, 100:10*sf:240) # microns
+    neffs_TM = ComplexF64[]
+    neffs_TE = ComplexF64[]
+    ns_bulk = ComplexF64[]
+    @showprogress for lam0 in lam0s
+        k0 = 2pi/lam0/1000 # [1/nm]
+        ep = LorentzDrudeMetals.Ti[lam0]
+        n = sqrt(ep)
+        push!(ns_bulk, n)
+        delta_s = lam0 / (-4pi*imag(n)) * 1000 # skin depth [nm]
+        msh = make_msh_hexlat_cyl(38, 38, 40, 40*sqrt(3), delta_s, 20)
+        m = Mesh(msh, [1.0,ep], [1.0,1.0])
+        neff = homogenize(m, k0, :TM)[1][1]
+        push!(neffs_TM, neff)
+        neff = homogenize(m, k0, :TE)[1][1]
+        push!(neffs_TE, neff)
+    end
+    write(f, "lam0s", lam0s)
+    write(f, "neffs_TM", neffs_TM)
+    write(f, "neffs_TE", neffs_TE)
+    write(f, "ns_bulk", ns_bulk)
+end
 
 
 ######################################
 ############# Figure 4a ##############
 ######################################
 
-# println("Begin Figure 4a; square lattice, d=38nm, L=40nm (G=2nm); different metals")
-# jldopen("./data/figure4a.jld", "w") do f
-#     lam0s = union(2:0.5*sf:10, 10:2*sf:50, 50:5*sf:100, 100:10*sf:240)
-#     write(f, "lam0s", lam0s)
-#     for (label,metal) in [("Al",LorentzDrudeMetals.Al),
-#                           ("Ag",LorentzDrudeMetals.Ag),
-#                           ("Au",LorentzDrudeMetals.Au),
-#                           ("Ti",LorentzDrudeMetals.Ti)]
-#         neffs = []
-#         @showprogress for lam0 in lam0s
-#             k0 = 2pi/lam0/1000 # [1/nm]
-#             ep = metal[lam0]
-#             n = sqrt(ep)
-#             delta_s = lam0 / (-4pi*imag(n)) * 1000 # skin depth [nm]
-#             @assert isapprox(delta_s, -1/2/imag(n)/k0)
-#             msh = make_msh_sqrlat_cyl(38, 38, 40, 40, delta_s, 20)
-#             m = Mesh(msh, [1.0,ep], [1.0,1.0])
-#             neff = homogenize(m, k0, :TE)[1][1] ##################### TODO CHANGE THIS TO TM ONCE WE FIX LABELS
-#             push!(neffs, neff)
-#         end
-#         write(f, "neffs_$(label)", neffs)
-#     end
-# end
+println("Begin Figure 4a; square lattice, d=38nm, L=40nm (G=2nm); different metals")
+jldopen("./data/figure4a.jld", "w") do f
+    lam0s = union(2:0.5*sf:10, 10:2*sf:50, 50:5*sf:100, 100:10*sf:240)
+    write(f, "lam0s", lam0s)
+    for (label,metal) in [("Al",LorentzDrudeMetals.Al),
+                          ("Ag",LorentzDrudeMetals.Ag),
+                          ("Au",LorentzDrudeMetals.Au),
+                          ("Ti",LorentzDrudeMetals.Ti)]
+        neffs = []
+        @showprogress for lam0 in lam0s
+            k0 = 2pi/lam0/1000 # [1/nm]
+            ep = metal[lam0]
+            n = sqrt(ep)
+            delta_s = lam0 / (-4pi*imag(n)) * 1000 # skin depth [nm]
+            @assert isapprox(delta_s, -1/2/imag(n)/k0)
+            msh = make_msh_sqrlat_cyl(38, 38, 40, 40, delta_s, 20)
+            m = Mesh(msh, [1.0,ep], [1.0,1.0])
+            neff = homogenize(m, k0, :TE)[1][1] ##################### TODO CHANGE THIS TO TM ONCE WE FIX LABELS
+            push!(neffs, neff)
+        end
+        write(f, "neffs_$(label)", neffs)
+    end
+end
 
 
 ######################################
